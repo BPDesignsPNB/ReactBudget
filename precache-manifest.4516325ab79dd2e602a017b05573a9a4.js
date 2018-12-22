d5432be55b3972c61ab67e2314f6e1590a6087c9
BPDesignsPNB <www.pablobeccalli@gmail.com>
2018-12-22 00:55:41 -0300

    Updates

---
+++ precache-manifest.4516325ab79dd2e602a017b05573a9a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65beef4ffe16ab1acbf7",
+    "url": "/ReactBudget/static/js/runtime~main.65beef4f.js"
+  },
+  {
+    "revision": "ed23702a92795d29eb65",
+    "url": "/ReactBudget/static/js/main.ed23702a.chunk.js"
+  },
+  {
+    "revision": "3daf17738b6a3d7c0929",
+    "url": "/ReactBudget/static/js/1.3daf1773.chunk.js"
+  },
+  {
+    "revision": "ed23702a92795d29eb65",
+    "url": "/ReactBudget/static/css/main.f11b4866.chunk.css"
+  },
+  {
+    "revision": "16b6f60cfe72fbbe39a3278b87e6aed0",
+    "url": "/ReactBudget/index.html"
+  }
+];
